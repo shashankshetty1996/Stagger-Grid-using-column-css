493f3a9d5430198abec3aefa5f0e08235c0d990e
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-03-16 21:02:11 +0530

    Add staggered using colums

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import axios from "axios";
+
+import Card from "./components/Card/Card";
+
 import "./App.css";
 
 class App extends Component {
+  state = {
+    data: [],
+    numberOfCard: 30
+  };
+
+  async componentDidMount() {
+    const jsonPlaceHolder = "https://jsonplaceholder.typicode.com";
+    try {
+      const photos = await axios.get(`${jsonPlaceHolder}/photos`);
+      const posts = await axios.get(`${jsonPlaceHolder}/posts`);
+      const data = this.generateData(photos.data, posts.data);
+      this.setState({ data });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  generateData = (photos, posts) => {
+    const { numberOfCard } = this.state;
+    let data = [];
+    for (let i = 0; i < numberOfCard; i++) {
+      if (i % 3 === 0) {
+        data.push({ ...photos[i], ...posts[i] });
+      } else {
+        data.push({ ...posts[i] });
+      }
+    }
+    return data;
+  };
+
   render() {
+    const { data } = this.state;
+    if (data.length === 0) {
+      return <h1>Loading data</h1>;
+    }
     return (
-      <div>
-        <h1>Staggered Grid POC</h1>
-      </div>
+      <Fragment>
+        <h1 className="title">Staggered Grid POC</h1>
+        <div className="container">
+          {data.map(card => (
+            <Card
+              key={card.id}
+              title={card.title}
+              url={card.url}
+              body={card.body}
+            />
+          ))}
+        </div>
+      </Fragment>
     );
   }
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Card = props => {
+  const { title, url, body } = props;
+  return (
+    <div className="card">
+      <div className="card-header">{title}</div>
+      <div className="card-content">
+        {url && <img src={url} alt="" />}
+        <p>{body}</p>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  data: PropTypes.object
+};
+
+Card.defaultProps = {
+  data: { title: "Card Title missing", body: "no data found", url: null }
+};
+
+export default Card;
